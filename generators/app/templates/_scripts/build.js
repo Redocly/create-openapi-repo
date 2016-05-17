466f060d023cfd2b100c0b1b1e7ee62c48ff9558
Roman Hotsiy <gotsijroman@gmail.com>
2016-05-17 18:58:53 +0300

    Fix deployment issue

--- generators/app/templates/_scripts/build.js
+++ generators/app/templates/_scripts/build.js
@@ -6,7 +6,7 @@ set('-e');
 
 mkdir('-p', 'web_deploy')
 
-cp('-R', 'web', 'web_deploy');
+cp('-R', 'web/*', 'web_deploy/');
 
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
