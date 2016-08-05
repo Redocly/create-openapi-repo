3720729bb39238d9a5eb051c4a0cc13acbb3059d
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2016-08-05 18:29:40 +0300

    Improve formating inside templates

--- generators/app/templates/_scripts/build.js
+++ generators/app/templates/_scripts/build.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 'use strict';
+<% if (installSwaggerUI) { -%>
+var Path = require('path');
+<% } -%>
 
 require('shelljs/global');
 set('-e');
@@ -11,10 +14,8 @@ cp('-R', 'web/*', 'web_deploy/');
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
-<% if (installSwaggerUI) { %>
-var Path = require('path');
+<% if (installSwaggerUI) { -%>
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
-SWAGGER_UI_DIST = '/Users/ivang/dev/generator-test/node_modules/swagger-ui/dist/'
 rm('-rf', 'web_deploy/swagger-ui/')
 cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '/swagger.json', 'web_deploy/swagger-ui/index.html')
