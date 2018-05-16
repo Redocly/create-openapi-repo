1bbe848708abcf7d69736ee3a3dd4e3e43dfb71f
Ondřej Lanč <ondrej.lanc@gmail.com>
2018-05-16 14:55:07 +0200

    Create swagger yaml file next to the json one also in not master branches.

--- generators/app/templates/_scripts/deploy-branch.js
+++ generators/app/templates/_scripts/deploy-branch.js
@@ -11,6 +11,7 @@ if (branch && branch !== 'gh-pages') {
   var branchPath = path.join('.tmp', 'preview', branch, '/');
   mkdir('-p', branchPath);
   exec('npm run swagger bundle -- -o ' + branchPath + 'swagger.json');
+  exec('npm run swagger bundle -- --yaml -o ' + branchPath + 'swagger.yaml');
   cp('web/index.html', branchPath);
   exec('deploy-to-gh-pages --update .tmp');
 }
