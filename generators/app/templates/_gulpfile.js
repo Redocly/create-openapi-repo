ee96c6e0fd0b90e24577082e07bdcbe2d5678a39
Runrioter Wung <runrioter@gmail.com>
2018-09-12 16:06:43 +0800

    refactor(remove): remove deprecated packages and update gulp to v4 (#26)

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,16 +1,43 @@
 var gulp = require('gulp');
-var util = require('gulp-util')
+var colors = require('ansi-colors');
+var log = require('fancy-log');
 var gulpConnect = require('gulp-connect');
 var connect = require('connect');
 var cors = require('cors');
-var path = require('path');
 var exec = require('child_process').exec;
 var portfinder = require('portfinder');
 var swaggerRepo = require('swagger-repo');
 
 var DIST_DIR = 'web_deploy';
 
-gulp.task('serve', ['build', 'watch', 'edit'], function() {
+function edit(done) {
+  portfinder.getPort({port: 5000}, function (err, port) {
+    var app = connect();
+    app.use(swaggerRepo.swaggerEditorMiddleware());
+    app.listen(port);
+    log(colors.green('swagger-editor started http://localhost:' + port));
+  });
+  done();
+}
+
+function build(done) {
+  exec('npm run build', function (err, stdout, stderr) {
+    console.log(stderr);
+    done(err);
+  });
+}
+
+function reload(done) {
+  gulp.src(DIST_DIR).pipe(gulpConnect.reload());
+  done();
+}
+
+function watch(done) {
+  gulp.watch(['spec/**/*', 'web/**/*'], gulp.series(build, reload));
+  done();
+}
+
+function start(done) {
   portfinder.getPort({port: 3000}, function (err, port) {
     gulpConnect.server({
       root: [DIST_DIR],
@@ -23,28 +50,7 @@ gulp.task('serve', ['build', 'watch', 'edit'], function() {
       }
     });
   });
-});
+  done();
+};
 
-gulp.task('edit', function() {
-  portfinder.getPort({port: 5000}, function (err, port) {
-    var app = connect();
-    app.use(swaggerRepo.swaggerEditorMiddleware());
-    app.listen(port);
-    util.log(util.colors.green('swagger-editor started http://localhost:' + port));
-  });
-});
-
-gulp.task('build', function (cb) {
-  exec('npm run build', function (err, stdout, stderr) {
-    console.log(stderr);
-    cb(err);
-  });
-});
-
-gulp.task('reload', ['build'], function () {
-  gulp.src(DIST_DIR).pipe(gulpConnect.reload())
-});
-
-gulp.task('watch', function () {
-  gulp.watch(['spec/**/*', 'web/**/*'], ['reload']);
-});
+exports.serve = gulp.series(build, start, watch, edit);
